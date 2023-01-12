cfe755f23b69f2e02eb627731103b921afcac422
jcarroyos <jcarroyos@gmail.com>
2023-01-12 16:47:46 -0500

    title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "Portfolio",
   tagline: "Dinosaurs are cool",
   url: "https://github.com",
   baseUrl: "/portfolio/",
