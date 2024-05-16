36cffad7d7979478ace276f2ea08c7fa45b414e7
Jc-Arroyo <jcarroyos@gmail.com>
2024-05-16 09:54:46 -0500

    Update gallery.js

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -8,11 +8,6 @@ export default function Hello() {
       title="My Gallery"
       description="paintings, sketches, drawings, doodles"
     >
-            <iframe height="600" style="width: 100%;" scrolling="no" title="Mastodon embed timeline (open to see it working)" src="https://codepen.io/jcarroyos/embed/MWdaOxv?default-tab=js%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
-  See the Pen <a href="https://codepen.io/jcarroyos/pen/MWdaOxv">
-  Mastodon embed timeline (open to see it working)</a> by Juan Carlos Arroyo (<a href="https://codepen.io/jcarroyos">@jcarroyos</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-</iframe>
       <Gallery />
     </Layout>
   );
