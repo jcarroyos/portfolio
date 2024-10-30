fc83cf4ffd98757bf6bfc0335106dd23704aa460
Jc-Arroyo <jcarroyos@gmail.com>
2024-10-30 02:24:52 +0000

    Update image links and navbar labels; refine Spanish translations, Bye bye amazon buckets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
             label: "Portfolio",
           },
           { to: "/blog", label: "Blog", position: "left" },
-          { to: "/gallery", label: "Work in progress", position: "left" },
+          { to: "/gallery", label: "WorkInProgress", position: "left" },
           { to: "/gallery-flickr", label: "Curated", position: "left" }
         ],
       },
